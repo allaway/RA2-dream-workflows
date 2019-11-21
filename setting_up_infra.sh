988b634c89ba4e293c0f29eaf28f8fa9eba48fcd
thomasyu888 <thomasyu888@gmail.com>
2019-11-21 13:23:09 -0800

    Fix typo

--- setting_up_infra.sh
+++ setting_up_infra.sh
@@ -10,11 +10,12 @@ pip install pyslurm --user
 # Start server locally... 
 # you need to start the server first, which will be serving on some host and port
 # then use the client to actually run stuff
-screen
+screen -S wes
+source ~/.bash_profile
 source activate cwl
 #wes-server --port 8082 --backend=wes_service.toil_wes --opt extra=--clean=never
 # Can't use /data/user... for some reason, singularity pull doesn't like it
-wes-server --backend=wes_service.cwl_runner --opt runner=cwltool --opt extra=--singularity --opt extra=--cachedir=/home/thomas.yu@sagebionetworks.org/cache_workflows/ --port 8082
+wes-server --backend=wes_service.cwl_runner --opt runner=cwltool --opt extra=--singularity --opt extra=--cachedir=$HOME/cache_workflows/ --port 8082
 #Use the key sequence Ctrl-a + Ctrl-d to detach from the screen session.
 #Use the key sequence Ctrl-a + H to obtain logs
 
@@ -23,7 +24,7 @@ export WES_API_AUTH='Header: value'
 export WES_API_PROTO=http
 
 git clone https://github.com/common-workflow-language/workflow-service.git
-cd workflow-servce
+cd workflow-service
 
 wes-client --info
 
@@ -46,17 +47,35 @@ export SINGULARITY_CACHEDIR=/data/scratch/thomas.yu@sagebionetworks.org/
 # Set up orchestrator
 
 # Use screen to allow for continous running
-screen -S orchestrator
+screen -S syn
 # Export all the values you use in your .env file
 # these values are explained above
-export WES_ENDPOINT=http://localhost:8082/ga4gh/wes/v1
-export WES_SHARED_DIR_PROPERTY=$USER_DATA/orchestrator/
-export SYNAPSE_USERNAME=ra2dreamservice
-export SYNAPSE_PASSWORD=xxxxxx
-export WORKFLOW_OUTPUT_ROOT_ENTITY_ID=syn20803806
-# Remember to put quotes around the EVALUATION_TEMPLATES
-export EVALUATION_TEMPLATES='{"9614319": "syn20976528"}'
-export COMPOSE_PROJECT_NAME=workflow_orchestrator
-export MAX_CONCURRENT_WORKFLOWS=4
+# export WES_ENDPOINT=http://localhost:8082/ga4gh/wes/v1
+# export WES_SHARED_DIR_PROPERTY=$USER_DATA/orchestrator/
+# export SYNAPSE_USERNAME=ra2dreamservice
+# export SYNAPSE_PASSWORD=xxxxxx
+# export WORKFLOW_OUTPUT_ROOT_ENTITY_ID=syn20803806
+# # Remember to put quotes around the EVALUATION_TEMPLATES
+# export EVALUATION_TEMPLATES='{"9614319": "syn20976528"}'
+# export COMPOSE_PROJECT_NAME=workflow_orchestrator
+# export MAX_CONCURRENT_WORKFLOWS=4
+source ~/.bash_profile
+source ~/.env
+java -jar WorkflowOrchestrator-1.0-SNAPSHOT-jar-with-dependencies.jar 
 
-java -jar 
+
+# Example slurm bash script
+
+#!/bin/bash
+#SBATCH --partition=express
+#SBATCH --job-name=test
+#SBATCH --time=02:00:00
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=thomas.yu@sagebionetworks.org
+#SBATCH --output=test.txt
+#SBATCH --error=test_errors.txt
+#SBATCH --mem=8G
+#SBATCH --gres=gpu:1
+#SBATCH --account=ra2_dream
+module load Singularity/2.6.1-GCC-5.4.0-2.26
+singularity exec --net --no-home --bind /cm/local/apps/cuda/libs --nv -B /data/scratch/thomas.yu@sagebionetworks.org/:/tmp:rw -B /data/project/RA2_DREAM/train:/train:ro -B /data/project/RA2_DREAM/test_leaderboard:/test:ro -B /home/thomas.yu@sagebionetworks.org/cache_workflows/3eed8f8f3fa5d73b971aea0ca1afa114:/output:rw docker://docker.synapse.org/syn20545112/example-model@sha256:6cc6dd92462b946fe5fbe0020055e63ce712c70e70fc327207cca6b26954b823 /run.sh
