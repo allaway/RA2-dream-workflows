3e1023f38d571bf715448bb06e7166ddcf3deb57
thomasyu888 <thomasyu888@gmail.com>
2019-11-21 13:29:46 -0800

    Update shell script

--- setting_up_infra.sh
+++ setting_up_infra.sh
@@ -13,38 +13,46 @@ pip install pyslurm --user
 screen -S wes
 source ~/.bash_profile
 source activate cwl
+
+export WES_API_HOST=localhost:8082
+export WES_API_AUTH='Header: value'
+export WES_API_PROTO=http
+
 #wes-server --port 8082 --backend=wes_service.toil_wes --opt extra=--clean=never
 # Can't use /data/user... for some reason, singularity pull doesn't like it
 wes-server --backend=wes_service.cwl_runner --opt runner=cwltool --opt extra=--singularity --opt extra=--cachedir=$HOME/cache_workflows/ --port 8082
 #Use the key sequence Ctrl-a + Ctrl-d to detach from the screen session.
 #Use the key sequence Ctrl-a + H to obtain logs
 
-export WES_API_HOST=localhost:8082
-export WES_API_AUTH='Header: value'
-export WES_API_PROTO=http
 
+
+# Example run with workflow service cwl files
 git clone https://github.com/common-workflow-language/workflow-service.git
 cd workflow-service
-
 wes-client --info
-
 wes-client --attachments="testdata/dockstore-tool-md5sum.cwl,testdata/md5sum.input" testdata/md5sum.cwl testdata/md5sum.cwl.json --no-wait
-
 wes-client --attachments="testtool.cwl,testtool.json" testtool.cwl testtool.json --no-wait
-
 wes-client --list
 
+# Example run with challeng workflow template cwl files
 git clone https://github.com/Sage-Bionetworks/ChallengeWorkflowTemplates.git
 cd ChallengeWorkflowTemplates
 wes-client scoring_harness_workflow.cwl scoring_harness_workflow.yaml  --attachments="download_submission_file.cwl,validate_email.cwl,validate.cwl,score.cwl,score_email.cwl,download_from_synapse.cwl,check_status.cwl,annotate_submission.cwl" --no-wait
 
-export SINGULARITY_TEMPDIR=/data/scratch/thomas.yu@sagebionetworks.org/
-export SINGULARITY_CACHEDIR=/data/scratch/thomas.yu@sagebionetworks.org/
+# export SINGULARITY_DOCKER_USERNAME=
+
+# export SINGULARITY_DOCKER_PASSWORD=
+# export SINGULARITY_BINDPATH=$USER_SCRATCH:/tmp
+# export TMPDIR=$USER_DATA
+# export SINGULARITY_CACHEDIR=$USER_SCRATCH/tmp
+# export SINGULARITY_PULLFOLDER=$USER_SCRATCH/tmp
+# export SINGULARITY_TEMPDIR=/data/scratch/thomas.yu@sagebionetworks.org/
 
 
 # curl localhost:8082/ga4gh/wes/v1/runs/<jobid>/cancel
 
-# Set up orchestrator
+
+### Set up orchestrator
 
 # Use screen to allow for continous running
 screen -S syn
